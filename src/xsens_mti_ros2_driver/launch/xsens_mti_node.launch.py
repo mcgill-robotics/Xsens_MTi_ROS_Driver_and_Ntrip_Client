51feea9ccb3b0d1e563faef744a5d5bf439fbcb5
avyan-k <avyan.kandasamy@gmail.com>
2026-04-20 17:23:38 +0000

    Make launch file prepend namespace and update documentation

--- src/xsens_mti_ros2_driver/launch/xsens_mti_node.launch.py
+++ src/xsens_mti_ros2_driver/launch/xsens_mti_node.launch.py
@@ -1,5 +1,6 @@
 from launch import LaunchDescription
-from launch.actions import SetEnvironmentVariable
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 from pathlib import Path
@@ -13,14 +14,25 @@ def generate_launch_description():
     ld.add_action(SetEnvironmentVariable('RCUTILS_LOGGING_BUFFERED_STREAM', '1'))
 
     parameters_file_path = Path(get_package_share_directory('xsens_mti_ros2_driver'), 'param', 'xsens_mti_node.yaml')
+    
+    namespace_arg = DeclareLaunchArgument(
+        'namespace',
+        default_value='imu',
+        description='Namespace for the node'
+    )
+    
+    ld.add_action(namespace_arg)
+
     xsens_mti_node = Node(
             package='xsens_mti_ros2_driver',
+            namespace=LaunchConfiguration('namespace'),
             executable='xsens_mti_node',
             name='xsens_mti_node',
             output='screen',
             parameters=[parameters_file_path],
             arguments=[]
             )
+    ld.add_action(namespace_arg)
     ld.add_action(xsens_mti_node)
 
     return ld
